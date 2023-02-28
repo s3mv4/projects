3d905094cdd5c8d06abb798850a2adeead752ca9
s3m <s3m@archlinux.org>
2023-02-28 14:04:13 +0100

    added navbar as a component

---
+++ javascript/react/components/navbar/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <div className="nav">
+                <h1 className="nav-title">Site</h1>
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default App
